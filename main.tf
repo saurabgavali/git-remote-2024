439ccd442e1922733f6ba1dcd12f2ff031db5fa3
TestUser <TestUser@example.com>
2024-06-10 14:16:34 +0530

    main.tf is upodated

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "terraform_test_vpc" {
   # cidr_block           = 172.31.0.0/16
   cidr_block         = "172.31.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_support   = false
  
   tags = {
     Name = "terraform_test_vpc"
