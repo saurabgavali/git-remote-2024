9a99133833d9078b52fe192ecf3025ca896bb440
saurabgavali <132080445+saurabgavali@users.noreply.github.com>
2024-06-10 20:34:48 +0530

    Update main.tf updated repo
    its updated

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 resource "aws_vpc" "terraform_test_vpc" {
-  # cidr_block           = 172.31.0.0/16
   cidr_block         = "172.31.0.0/16"
   enable_dns_hostnames = false
   enable_dns_support   = false
  
   tags = {
-    Name = "terraform_test_vpc"
+    Name = "tr_test_vpc"
   }
 }
