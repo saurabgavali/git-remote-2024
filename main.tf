837e541fdaa6c7d149a1ad2e7bce32888b96c188
TestUser <TestUser@example.com>
2024-06-10 20:02:28 +0530

    modify proprty

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "terraform_test_vpc" {
   # cidr_block           = 172.31.0.0/16
   cidr_block         = "172.31.0.0/16"
-  enable_dns_hostnames = true
+  enable_dns_hostnames = false
   enable_dns_support   = false
  
   tags = {
