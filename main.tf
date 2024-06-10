e9395af77b6e5cab6856df9412fb108a48dda47c
TestUser <TestUser@example.com>
2024-06-10 14:05:41 +0530

    add main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "terraform_test_vpc" {
+  # cidr_block           = 172.31.0.0/16
+  cidr_block         = "172.31.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+ 
+  tags = {
+    Name = "terraform_test_vpc"
+  }
+}
